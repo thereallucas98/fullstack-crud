0f9024c55486bb025143e2f0383410195149580c
thereallucas98 <lucas1998david@gmail.com>
2022-11-10 20:51:11 -0300

    [frontend]: responsive header

--- web/src/components/Header/Profile.tsx
+++ web/src/components/Header/Profile.tsx
@@ -1,15 +1,21 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
   return (
     <Flex align="center" ml="auto">
       <Flex align="center">
-        <Box mr="4" textAlign="right">
-          <Text>David Lucas</Text>
-          <Text color="gray.300" fontSize="small">
-            david.lucas@snet.com.br
-          </Text>
-        </Box>
+        {showProfileData ? (
+          <Box mr="4" textAlign="right">
+            <Text>David Lucas</Text>
+            <Text color="gray.300" fontSize="small">
+              david.lucas@snet.com.br
+            </Text>
+          </Box>
+        ) : null}
 
         <Avatar size="md" name="David Lucas" />
       </Flex>
