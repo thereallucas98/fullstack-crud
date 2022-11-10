a12d69561471399970c32744b09aabd1c15e2180
thereallucas98 <lucas1998david@gmail.com>
2022-11-10 18:59:46 -0300

    [frontend]: login page formControl

--- web/src/pages/public/Login.tsx
+++ web/src/pages/public/Login.tsx
@@ -1,4 +1,11 @@
-import { Button, Flex, Input, Stack, FormLabel } from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  Input,
+  Stack,
+  FormLabel,
+  FormControl,
+} from "@chakra-ui/react";
 
 export function Login() {
   return (
@@ -13,33 +20,37 @@ export function Login() {
         flexDir="column"
       >
         <Stack spacing="4">
-          <FormLabel htmlFor="email">E-mail</FormLabel>
-          <Input
-            id="email"
-            name="email"
-            type="email"
-            focusBorderColor="blue.500"
-            bgColor="gray.900"
-            variant="filled"
-            _hover={{
-              bgColor: "gray.900",
-            }}
-            size="lg"
-          />
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              id="email"
+              name="email"
+              type="email"
+              focusBorderColor="blue.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: "gray.900",
+              }}
+              size="lg"
+            />
+          </FormControl>
 
-          <FormLabel htmlFor="password">Senha</FormLabel>
-          <Input
-            id="password"
-            name="password"
-            type="password"
-            focusBorderColor="blue.500"
-            bgColor="gray.900"
-            variant="filled"
-            _hover={{
-              bgColor: "gray.900",
-            }}
-            size="lg"
-          />
+          <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+            <Input
+              id="password"
+              name="password"
+              type="password"
+              focusBorderColor="blue.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: "gray.900",
+              }}
+              size="lg"
+            />
+          </FormControl>
         </Stack>
 
         <Button type="submit" mt="6" colorScheme="blue" size="lg">
