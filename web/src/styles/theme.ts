72cdc28a2b2ce1f7e4312434747e0338e0d63798
thereallucas98 <lucas1998david@gmail.com>
2022-11-13 19:18:19 -0300

    [frontend]: configured eslint for styles

--- web/src/styles/theme.ts
+++ web/src/styles/theme.ts
@@ -1,42 +1,42 @@
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme } from '@chakra-ui/react'
 
 export const theme = extendTheme({
   components: {
     Modal: {
       baseStyle: () => ({
         dialog: {
-          bg: '#1f2029'
-        }
-      })
-    }
+          bg: '#1f2029',
+        },
+      }),
+    },
   },
   colors: {
     gray: {
-      "900": "#181b23",
-      "800": "#1f2029",
-      "700": "#353646",
-      "600": "#4b4d63",
-      "500": "#616480",
-      "400": "#797d9a",
-      "300": "#9699b0",
-      "200": "#b3b5c6",
-      "100": "#d1d2dc",
-      "50":  "#eeeef2"
+      '900': '#181b23',
+      '800': '#1f2029',
+      '700': '#353646',
+      '600': '#4b4d63',
+      '500': '#616480',
+      '400': '#797d9a',
+      '300': '#9699b0',
+      '200': '#b3b5c6',
+      '100': '#d1d2dc',
+      '50': '#eeeef2',
     },
     blue: {
-      "900": "#3182ce",
-    }
+      '900': '#3182ce',
+    },
   },
   fonts: {
     heading: 'Roboto',
-    body:  'Roboto',
+    body: 'Roboto',
   },
   styles: {
     global: {
       body: {
         bg: 'gray.900',
-        color: 'gray.50'
-      }
-    }
-  }
+        color: 'gray.50',
+      },
+    },
+  },
 })
