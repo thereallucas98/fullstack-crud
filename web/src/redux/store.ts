fcc7065abf301b86ae7640b3c6b7c2889bf9a60f
thereallucas98 <lucas1998david@gmail.com>
2022-11-13 19:17:51 -0300

    [frontend]: configured eslint for redux

--- web/src/redux/store.ts
+++ web/src/redux/store.ts
@@ -1,29 +1,28 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import { serverApi } from "../api/server.service";
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+import { serverApi } from '../api/server.service'
 
-import authReducer from "./auth";
+import authReducer from './auth'
 
 const reducer = combineReducers({
   auth: authReducer,
   [serverApi.reducerPath]: serverApi.reducer,
-});
+})
 
-const customMiddleware = [
-  serverApi.middleware,
-]
+const customMiddleware = [serverApi.middleware]
 
 const store = configureStore({
   reducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(customMiddleware),
-});
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(customMiddleware),
+})
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch;
+export type AppDispatch = typeof store.dispatch
 
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch = () => useDispatch<AppDispatch>()
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
 
-export default store;
+export default store
