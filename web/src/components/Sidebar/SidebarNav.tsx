abe5e27c37874865418ca9d671acb530b73469e9
thereallucas98 <lucas1998david@gmail.com>
2022-11-13 21:12:48 -0300

    [frontend]: temporary improvement for logout

--- web/src/components/Sidebar/SidebarNav.tsx
+++ web/src/components/Sidebar/SidebarNav.tsx
@@ -1,7 +1,8 @@
 import { Button, Stack } from '@chakra-ui/react'
+import { useNavigate } from 'react-router-dom'
 import { House, SignOut, User, Users } from 'phosphor-react'
 import { useCallback } from 'react'
-import { logout, setCredentials } from '../../redux'
+import { logout } from '../../redux'
 import { useAppDispatch } from '../../redux/store'
 
 import { NavLink } from './NavLink'
@@ -9,10 +10,12 @@ import { NavSection } from './NavSection'
 
 export function SidebarNav() {
   const dispatch = useAppDispatch()
+  const navigate = useNavigate()
 
   const handleSignOut = useCallback(() => {
     dispatch(logout({ user: null, token: null }))
-  }, [dispatch])
+    navigate('/')
+  }, [dispatch, navigate])
 
   return (
     <Stack spacing="12" align="flex-start">
